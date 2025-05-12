4aa2f18d4049d395bd8c63c3f077eacbaa8899e2
milan777777 <milangthapa07@gmail.com>
2025-05-12 15:19:40 +0545

    extracting file

--- extract/network2.py
+++ extract/network2.py
@@ -1,29 +1,29 @@
 import zipfile
-import ijson #for large dataset
-import json #supports json file 
-import os # helps in file navigation,helps to make directory 
-from decimal import Decimal # for decimal calculation to change decimal into float
+import ijson 
+import json  
+import os 
+from decimal import Decimal
 
-def extract_import(zip_path): #define function 
-    def con_decimal(obj): # for managing decimal value turning decimal into float 
+def extract_import(zip_path):
+    def con_decimal(obj): 
         if isinstance(obj, Decimal):
             return float(obj)
-        raise TypeError #if comes error in above condition comes type error 
+        raise TypeError  
     
-    output_path = os.path.join(os.getcwd(),'output_file')#makes working directory 
-    os.makedirs(output_path, exist_ok=True) #helps to make folder and put in output path(os.ma is a directory)
+    output_path = os.path.join(os.getcwd(),'output_file')
+    os.makedirs(output_path, exist_ok=True) 
 
-    in_path = os.path.join(output_path, 'in_network.json')#in_path for in_network.json(only path)
-    prov_path = os.path.join(output_path, 'provider_ref.json')#prov_path for provider.json
+    in_path = os.path.join(output_path, 'in_network.json')
+    prov_path = os.path.join(output_path, 'provider_ref.json')
 
-    with zipfile.ZipFile(zip_path, 'r') as z:#reading the zipfile and z is alias,importing the zipfile package
-        for data in z.namelist():#we move to the directory and namelist gives all the files in the directory
-            with z.open(data, 'r') as f:# r helps to read the file 
-                with open(in_path,'w') as in_file:# w helps to write the file,
+    with zipfile.ZipFile(zip_path, 'r') as z:
+        for data in z.namelist():
+            with z.open(data, 'r') as f:
+                with open(in_path,'w') as in_file:
                     for item in ijson.items(f, 'in_network.item'):
-                        in_file.write(json.dumps(item, default=con_decimal) + '\n')#json.dump helps to convert python file into json format and call above function 
+                        in_file.write(json.dumps(item, default=con_decimal) + '\n')
 
-                f.seek(0)# gives timeout message if file not found 
+                f.seek(0)
 
                 with open(prov_path,'w') as prov_file:
                     for item in ijson.items(f, 'provider_references.item'):
