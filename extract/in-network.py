500637f776fd60bd93df13837edec9127175d3c0
milan777777 <milangthapa07@gmail.com>
2025-05-27 17:02:11 +0545

    jbndjnb

--- extract/in-network.py
+++ extract/in-network.py
@@ -17,18 +17,6 @@ print(rate_df)
 rate_df.to_json('rate.json',orient='records',indent=3)
 rate_df.to_parquet('rate.parquet',index=False)
 
-# import ijson
-# import zipfile
-# import json
-# import decimal as Decimal 
 
-# with open('example.json','w') as out_file:
-#     with zipfile.ZipFile('/home/milan-thapa/Downloads/MagnaCarePPO_In-Network.zip', 'r') as zf:
-#         for name in zf.namelist():
-#             with zf.open(name, 'r') as f:
-#                 print(name)
-#                 for item in ijson.items(f, 'provider_references.item'):
-  
-#                     out_file.write(json.dumps(item) + '\n')
 
 
